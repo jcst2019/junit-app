4e8a3b7cdb7869bffcdff5f515ce4c267c51fca8
jcst2019 <juansolar_4@hotmail.com>
2024-05-15 00:22:37 -0500

    Crear Clase Cuenta

---
+++ src/main/java/org/solaris/junit5app/ejemplos/models/Cuenta.java
@@ -0,0 +1,26 @@
+package org.solaris.junit5app.ejemplos.models;
+
+import java.math.BigDecimal;
+
+public class Cuenta {
+
+    private String persona;
+    private BigDecimal saldo;
+
+    public String getPersona() {
+        return persona;
+    }
+
+    public void setPersona(String persona) {
+        this.persona = persona;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+
+}
